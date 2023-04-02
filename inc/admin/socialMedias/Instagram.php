340c36c381b767739d4b327cbc5cfc538e98874b
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2023-04-03 02:13:53 +0330

    add instagram class

---
+++ inc/admin/socialMedias/Instagram.php
@@ -0,0 +1,29 @@
+<?php 
+
+class Instagram{
+
+    public function __construct() {
+        $this->register();
+        add_action('init',[$this,'register']);
+    }
+
+    public function register(){
+        register_setting('social-media-settings-group', 'social_media_instagram');
+        add_settings_field(
+            'social-media-instagram',
+            'instagram',
+            [$this,'social_media_instagram_callback'],
+            'social-media',
+            'social_media'
+          );
+    }
+    public function social_media_instagram_callback(){
+        $value = get_option('social_media_instagram');
+        echo "<input placeholder='Instagram link' type='text' name='social_media_instagram' value='$value' />";
+    }
+    public static function GetOptioninstagram(){
+        $instagram = get_option('social_media_instagram');
+        echo $instagram;
+    }
+}
+new Instagram();
