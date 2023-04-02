429663d43a31053d2cdb3e0c10edf94d6b9d2884
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2023-04-02 15:54:10 +0330

    add main file

---
+++ Social.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+Plugin Name:  Social Media Icons
+Plugin URI: #
+Description: This plugin allows you to add social media icons to your application and web pages
+Author: Parsa Mirzaie
+Author URI: https://eskanogroup.com
+License: GPL2
+*/
