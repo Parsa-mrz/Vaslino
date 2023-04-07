f6570d1c51d6306eb93ef28bd49a9062670175eb
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2023-04-08 00:29:55 +0330

    update templates

--- inc/user/templates/social_icons.php
+++ inc/user/templates/social_icons.php
@@ -14,26 +14,23 @@ $links = [
           <link rel="stylesheet" href="<?= SOC_URL . 'assets/css/style.css' ?>">
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
       </head>
-      <body>  
-        <div class="social-dropdown">
-          <button class="social-dropbtn"><i class="fa fa-share-nodes"></i></button>
-          <div class="social-dropdown-content">
-            <?php
-              if($links['facebook']){?>
+      <div class="sec-center"> 	
+        <input class="dropdown" type="checkbox" id="dropdown" name="dropdown"/>
+        <label class="for-dropdown" for="dropdown">Social Media<i class="uil uil-arrow-down"></i></label>
+          <div class="section-dropdown"> 
+          <?php
+        if ($links['facebook']) {?>
                   <a href="<?= 'https://' . $links['facebook'] ?>"><i class="fa-brands fa-facebook"></i><?php _e('Facebook')?></a>
                 <?php
-              }if($links['twitter']){?>
+        }if ($links['twitter']) {?>
                   <a href="<?= 'https://' . $links['twitter'] ?>"><i class="fa-brands fa-twitter"></i><?php _e('Twitter')?></a>
                 <?php
-              }if($links['instagram']){?>
+        }if ($links['instagram']) {?>
                   <a href="<?= 'https://' . $links['instagram'] ?>"><i class="fa-brands fa-instagram"></i><?php _e('Instagram')?></a>
                 <?php
-              }
-            ?>
-          </div>
-        </div>
-      <script src="<?= SOC_URL . '/assets/js/script.js'?>" ></script>
-      </body>
-      <?php 
-    }
-  ?>
+        }
+    ?>
+         </div>
+      </div>
+<?php
+ }
